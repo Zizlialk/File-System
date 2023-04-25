f2bf58d5d0ccc10ceda7ac277d7ab9a577bb2a9f
Zizlialk <alexander.ian.beattie@gmail.com>
2023-04-25 17:16:01 -0400

    Added many of the methods that will be part of the directory for now

--- main.cpp
+++ main.cpp
@@ -3,17 +3,71 @@
 
 class file
 {
-    public:
-        int NoContent;
-        
-};
+	public:
+		int NoContent;
+		bool Dir;               //Is this file a directory
+		std::string name;       //Name of the file. Directory or otherwise
+		std::string Contents;   //the contentst of a single file/
 
-class Directory
-{
-    public:
         file parent;
         file child;
         file sibling;
+
+		file(std::string name, bool dir){
+			//Contructor
+			this->name = name;      //name of the file
+			this->Contents = "";    //nothing there yet. empty file basically
+			this->Dir = dir;        //Is this file a directory file
+			this->NoContent = 0;
+		}
+	
+		std::string Print()
+		{
+			//Printing the information of the file
+			return this->name; //Should be enough for the time being. there might be another version with something like ls -l
+		}
+
+	};
+
+class Directory
+{
+    file head;          //This is the root of the directory
+
+    public:
+
+
+		int New(std::string name){  //return 0 : worked, -1 : failure
+			//name
+			//location
+			//Directory?
+			//parent
+			//Sibling would be listm anipulation
+			//child there are none yet.
+
+
+		}
+
+		int changeDir(){
+			//Chaning the working directory
+		}
+		int Delete(){
+			//remove a file, need to decide on unix or windows version
+		}
+
+		void traversal(){
+			//Some way to traverse the whole directory
+			file cur = head;	//Getting the head of the directory to traverse through
+			Intraversal(cur);
+		}
+		void interact(){
+			//This will be where things like writing to a file go
+
+		}
+	private:
+		std::string Intraversal(file cur)
+		{
+			Intraversal(cur.sibling)
+		}
 
 };
 
