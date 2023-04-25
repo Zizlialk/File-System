0e206f122785f0ec510707af488942d7b80c6e62
Zizlialk <alexander.ian.beattie@gmail.com>
2023-04-25 15:48:56 -0400

    First proper commit, +main.cpp -test push.txt

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class file
+{
+    public:
+        int NoContent;
+        
+};
+
+class Directory
+{
+    public:
+        file parent;
+        file child;
+        file sibling;
+
+};
+
+int main()
+{
+
+    return 0;
+}
